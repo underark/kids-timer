13febe171917996dde6d0c7ff159030086203b23
alex <peter.alex.morris@gmail.com>
2026-06-12 12:14:23 +0900

    Added form with time input and javscript to collect time data

--- js.js
+++ js.js
@@ -1,1 +1,9 @@
-console.log("Hello, world!");
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const data = new FormData(form);
+        const date = data.get("time");
+        console.log(date);
+    })
+})
