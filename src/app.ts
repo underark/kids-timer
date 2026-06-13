f3aab39dcaaa7b2d2df7e84f11cf1a38b9f3f3aa
alex <peter.alex.morris@gmail.com>
2026-06-13 22:06:05 +0900

    Refactored code for parsing new date with user's input time - simplified code by cloning existing date and modifying time with setHours method

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const data = new FormData(form);
         const time = data.get("time")?.toString();
         const timerEnd = getDateFromTime(new Date(), time!);
+        console.log(timerEnd);
         const duration = getTimeDiff(new Date(), timerEnd);
         console.log(duration);
     });
 });
 
-// getDateFromTime expects a string in the format HH:MM, as is returned from a time form input
-// getDateFromTime expects a Date object using a Date constructor
-// getDateFromTime returns a Date object
 function getDateFromTime(date: Date, time: string): Date {
-    // getMonth returns a 0 indexed month (0 for January) but the parser expects a 1-12 range
-    // the Date parser expects a leading 0 before all single digits
-    const month = (date.getMonth() + 1 < 10) ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
-    const day = (date.getDate() < 10) ? `0${date.getDate()}` : `${date.getDate()}`;
-    const dateTimeString = `${date.getFullYear()}-${month}-${day}T${time}:00`;
-    return new Date(dateTimeString);
+    const dateClone = new Date(date.valueOf());
+    const timeParts: string[] = time.split(":");
+    dateClone.setHours(parseInt(timeParts[0]!), parseInt(timeParts[1]!));
+    return dateClone;
 }
 
 // getTimeDiff expects two Date objects, the start time and the end time of the timer
