4ec0f637217fa6d2ba40226768da8a4b84551b7d
alex <peter.alex.morris@gmail.com>
2026-06-14 16:00:52 +0900

    Fixed rendering timing logic to use previous timestamps instead of Date.now unix epoch time

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,44 @@
 import { Timer } from "../timer/timer.js";
 
 type ActiveTimer =
-    | { running: true; timer: Timer }
-    | { running: false; timer: null }
+    | { running: true; value: Timer }
+    | { running: false; value: null }
 
 class App {
     timer: ActiveTimer;
+    lastTimestamp: number;
 
     constructor() {
-        this.timer = { running: false, timer: null };
+        this.timer = { running: false, value: null };
+        this.lastTimestamp = 0;
     }
 
     i_startTimer(end: string) {
         const timer = new Timer(new Date(), end);
-        this.timer = { running: true, timer: timer };
+        this.timer = { running: true, value: timer };
         const i_render = (timestamp: number) => {
-            const sinceLast = Date.now() - timestamp;
-            if (this.timer.running === true && sinceLast >= 16) {
+            if (this.timer.running === true && this.diffLastTimestamp(timestamp) >= 16) {
                 const div = document.querySelector("#progress");
-                const elapsed = this.timer.timer.calculateElapsed();
+                const elapsed = this.timer.value.calculateElapsed();
                 div!.textContent = elapsed.toString();
                 if (elapsed >= 100) {
-                    this.timer = { running: false, timer: null };
+                    this.i_cancelTimer();
                     return;
                 }
+                this.lastTimestamp = timestamp;
             }
             requestAnimationFrame(i_render);
         };
         requestAnimationFrame(i_render);
+    }
+
+    i_cancelTimer() {
+        this.timer = { running: false, value: null };
+        this.lastTimestamp = 0;
+    }
+
+    diffLastTimestamp(timestamp: DOMHighResTimeStamp) {
+        return timestamp - this.lastTimestamp;
     }
 }
 
